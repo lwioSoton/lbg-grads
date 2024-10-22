0af13b7f02646fa5b940ae25a07c096130d25862
Oscar Ip <lwi1g19@soton.ac.uk>
2024-10-22 15:12:44 +0100

    EDIT Q6-12 sakila-requests.sql

--- week2/sample_databases/sakila/sakila-requests.sql
+++ week2/sample_databases/sakila/sakila-requests.sql
@@ -25,3 +25,52 @@ FROM film
 HAVING film_id = 100;
 
 #Q6
+SELECT * 
+FROM film
+HAVING rating = "R";
+
+#Q7
+SELECT * 
+FROM film
+HAVING rating != "R";
+
+#Q8
+SELECT * 
+FROM film
+ORDER BY length ASC
+LIMIT 10;
+
+#Q9
+SELECT * 
+FROM film
+#GROUP BY length
+HAVING length > 180
+ORDER BY length DESC;
+
+#Q10
+SELECT * 
+FROM film
+HAVING special_features LIKE "%Deleted Scenes%";
+
+#Q11, 12 Test
+SELECT 
+	last_name
+FROM actor
+ORDER BY last_name DESC;
+
+#Q11
+SELECT 
+	last_name
+FROM actor
+GROUP BY last_name
+HAVING COUNT(last_name) = 1 
+ORDER BY last_name DESC;
+
+#Q12
+SELECT 
+	last_name,
+    COUNT(last_name)
+FROM actor
+GROUP BY last_name
+HAVING COUNT(last_name) >= 2
+ORDER BY COUNT(last_name) DESC;
